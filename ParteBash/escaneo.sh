18112eddac19519fdb84e36146beae51a090102a
TeyssiHM <anateyssi.hernandezez@uanl.edu.mx>
2024-11-11 08:10:26 -0600

    Update escaneo.sh

--- ParteBash/escaneo.sh
+++ ParteBash/escaneo.sh
@@ -4,7 +4,16 @@ escaneo(){
     
 # Inicializa una lista para los hosts activos
 hosts_activos=()
- 
+
+#Reporte
+REPORTE= "reporte_escaneo_red.txt"
+
+#Mostrar informacion sobre el archivo
+
+echo "Feche y hora: $(date)" >> "REPORTE_FILE"
+echo "------------------------------------------" >> "$REPORTE"
+
+
 # Bucle para escanear los hosts
 for i in {1..255}; do
     if timeout 2 ping -c 1 192.168.56.$i >/dev/null 2>&1; then
